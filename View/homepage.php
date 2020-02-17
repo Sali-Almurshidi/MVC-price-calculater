dfea7b19668be5d53a844d1115f80d0eb960de25
sali <salialmurshidi@gmail.com>
2020-02-17 12:19:09 +0100

    i am  sorry

--- View/homepage.php
+++ View/homepage.php
@@ -1,3 +1,15 @@
+<?php
+declare(strict_types=1);
+
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+
+$allData = new User();
+$allCustomersArray = $allData->getAllCustomers();
+
+var_dump($allCustomersArray);
+?>
 <!doctype html>
 <html lang="en">
 <head>
