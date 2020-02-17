19e681cfd500d6dc798cc6a25bfa7174fc41c737
sali <salialmurshidi@gmail.com>
2020-02-17 12:17:45 +0100

    merge

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-
 declare(strict_types=1);
-
-ini_set('display_errors', "1");
-ini_set('display_startup_errors', "1");
-error_reporting(E_ALL);
 
 //include all your model files here
 require 'Model/User.php';
@@ -12,14 +7,9 @@ require 'Model/User.php';
 require 'Controller/HomepageController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
-//this file should never be more than 20 lines of code!
-/*$controller = new HomepageController();
-$controller->render($_GET, $_POST);*/
-
-$allData = new User();
-$allCustomersArray = $allData->getAllCustomers();
-
-var_dump($allCustomersArray);
+//this file should never be more than 20 lines of code
+$controller = new HomepageController();
+$controller->render($_GET, $_POST);
 
 ?>
 
