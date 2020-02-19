6517c6c0b453528f610d1b18d91616c55250ca7d
Chihyu <chihyu10@gmail.com>
2020-02-19 09:06:32 +0100

    push to pull

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -31,7 +31,7 @@ class HomepageController
                     $_SESSION["productID"] = $_POST["Product"];
                     $_SESSION["customerID"] = $_POST["Customer"];
                     // move to next page
-                    header("Location: http://pricecalculator.local/View/selectionresult.php ");
+                    //header("Location: http://pricecalculator.local/View/selectionresult.php ");
                 } else {
                     echo "Select the Product name and the Customer Name";
                 }
