9e82ae506aa2ec1cef3350445ed7def57b4835ce
Chihyu <chihyu10@gmail.com>
2020-02-19 09:05:38 +0100

    Pushing to pull

--- View/selectionresult.php
+++ View/selectionresult.php
@@ -9,7 +9,22 @@ require '../Model/Groups.php';
 //include all your controllers here
 require '../Controller/HomepageController.php';
 
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
 
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+whatIsHappening();
 
 $customerID = $_SESSION['customerID'];
 $productsID = $_SESSION['productID'];
